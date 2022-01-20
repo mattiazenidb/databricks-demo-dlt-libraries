1c3aaa8cd5fb7b6daa2b8eff2591dc234e436546
mattiazenidb <mattiazenidb>
2022-01-20 17:11:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+as_int(3)
+
+# COMMAND ----------
+
+# MAGIC %run ./modules_folder/setup
+
+# COMMAND ----------
+
+as_int(3)
---
+++ modules_folder/setup.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+#!/usr/bin/env python3
+
+# Exported function
+def as_int(a):
+    return int(a)
+
+# Test function for module  
+def _test():
+    assert as_int('1') == 1
+
+if __name__ == '__main__':
+    _test()
